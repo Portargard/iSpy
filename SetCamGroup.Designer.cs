ed22de2c2024b5c4a489f208583276833c9b21be
Portargard <portargard@gmail.com>
2024-10-15 13:11:34 +0700

    Manager Camera Group

---
+++ SetCamGroup.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace iSpyApplication
+{
+    partial class SetCamGroup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trv_Data = new System.Windows.Forms.TreeView();
+            this.btn_confirm = new System.Windows.Forms.Button();
+            this.tbx_NewNode = new System.Windows.Forms.TextBox();
+            this.lbl_NewNode = new System.Windows.Forms.Label();
+            this.btn_RemoveNode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // trv_Data
+            // 
+            this.trv_Data.Location = new System.Drawing.Point(12, 12);
+            this.trv_Data.Name = "trv_Data";
+            this.trv_Data.Size = new System.Drawing.Size(776, 288);
+            this.trv_Data.TabIndex = 0;
+            // 
+            // btn_confirm
+            // 
+            this.btn_confirm.Location = new System.Drawing.Point(349, 378);
+            this.btn_confirm.Name = "btn_confirm";
+            this.btn_confirm.Size = new System.Drawing.Size(108, 42);
+            this.btn_confirm.TabIndex = 1;
+            this.btn_confirm.Text = "Add";
+            this.btn_confirm.UseVisualStyleBackColor = true;
+            this.btn_confirm.Click += new System.EventHandler(this.btn_confirm_Click);
+            // 
+            // tbx_NewNode
+            // 
+            this.tbx_NewNode.Location = new System.Drawing.Point(349, 322);
+            this.tbx_NewNode.Name = "tbx_NewNode";
+            this.tbx_NewNode.Size = new System.Drawing.Size(177, 22);
+            this.tbx_NewNode.TabIndex = 2;
+            // 
+            // lbl_NewNode
+            // 
+            this.lbl_NewNode.AutoSize = true;
+            this.lbl_NewNode.Location = new System.Drawing.Point(243, 322);
+            this.lbl_NewNode.Name = "lbl_NewNode";
+            this.lbl_NewNode.Size = new System.Drawing.Size(99, 16);
+            this.lbl_NewNode.TabIndex = 3;
+            this.lbl_NewNode.Text = "Add New Node";
+            // 
+            // btn_RemoveNode
+            // 
+            this.btn_RemoveNode.Location = new System.Drawing.Point(479, 378);
+            this.btn_RemoveNode.Name = "btn_RemoveNode";
+            this.btn_RemoveNode.Size = new System.Drawing.Size(108, 42);
+            this.btn_RemoveNode.TabIndex = 4;
+            this.btn_RemoveNode.Text = "Remove Node";
+            this.btn_RemoveNode.UseVisualStyleBackColor = true;
+            this.btn_RemoveNode.Click += new System.EventHandler(this.btn_RemoveNode_Click);
+            // 
+            // SetCamGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_RemoveNode);
+            this.Controls.Add(this.lbl_NewNode);
+            this.Controls.Add(this.tbx_NewNode);
+            this.Controls.Add(this.btn_confirm);
+            this.Controls.Add(this.trv_Data);
+            this.Name = "SetCamGroup";
+            this.Text = "SetCamGroup";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SetCamGroup_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView trv_Data;
+        private System.Windows.Forms.Button btn_confirm;
+        private System.Windows.Forms.TextBox tbx_NewNode;
+        private System.Windows.Forms.Label lbl_NewNode;
+        private System.Windows.Forms.Button btn_RemoveNode;
+    }
+}
---
+++ SetCamGroup.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace iSpyApplication
+{
+    public partial class SetCamGroup : Form
+    {
+        private bool setCamMode;
+        public string nodeData;
+
+        public SetCamGroup(bool setGroupForCam)
+        {
+            InitializeComponent();
+            LoadData();
+            setCamMode = setGroupForCam;
+            if (setGroupForCam)
+            {
+                tbx_NewNode.Visible = false;
+                lbl_NewNode.Visible = false;
+                btn_RemoveNode.Visible = false;
+                btn_confirm.Text = "OK";
+                this.Text = "Set Camera Group";
+            }
+            else { this.Text = "Manager Camera Group"; }
+        }
+
+        private void btn_confirm_Click(object sender, EventArgs e)
+        {
+            if (setCamMode) 
+            { 
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+                nodeData = trv_Data.SelectedNode.Text;
+            }
+            else
+            {
+                if (trv_Data.SelectedNode != null && tbx_NewNode.Text.Replace(" ","").Count()!=0)
+                {
+                    // Tạo node mới
+                    TreeNode newNode = new TreeNode(tbx_NewNode.Text);
+
+                    // Thêm node mới vào node đã chọn
+                    trv_Data.SelectedNode.Nodes.Add(newNode);
+
+                    // Mở rộng node cha để hiển thị node con
+                    trv_Data.SelectedNode.Expand();
+                    trv_Data.SelectedNode = null;
+                    tbx_NewNode.Clear();
+                }
+                else
+                {
+                    if (tbx_NewNode.Text.Replace(" ", "").Count() != 0)
+                    {
+                        TreeNode newNode = new TreeNode(tbx_NewNode.Text);
+                        trv_Data.Nodes.Add(newNode);
+                    }
+              }
+            }
+        }
+        private void LoadData()
+        {
+            string filePath = Program.AppDataPath + @"XML/CamGroup.xml";
+            var treeNodesData = LoadTreeViewFromXml(filePath);
+            LoadDataToTreeView(treeNodesData);
+
+        }
+        private void SaveData()
+        {
+            string filePath = Program.AppDataPath + @"XML/CamGroup.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(TreeNodeData[]));
+
+            TreeNodeData[] nodesData = GetTreeNodeData(trv_Data.Nodes);
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(stream, nodesData);
+            }
+        }
+
+        private void btn_RemoveNode_Click(object sender, EventArgs e)
+        {
+            if (trv_Data.SelectedNode != null)
+            {
+                // Lưu lại node cha của node đã chọn
+                TreeNode parentNode = trv_Data.SelectedNode.Parent;
+
+                // Xoá node đã chọn
+                trv_Data.Nodes.Remove(trv_Data.SelectedNode);
+
+                // Hoặc nếu bạn muốn xoá node trong trường hợp nó có parent:
+                //parentNode.Nodes.Remove(trv_Data.SelectedNode);
+            }
+        }
+
+        #region function
+        //save
+        private TreeNodeData[] GetTreeNodeData(TreeNodeCollection nodes)
+        {
+            List<TreeNodeData> nodeDataList = new List<TreeNodeData>();
+
+            foreach (TreeNode node in nodes)
+            {
+                TreeNodeData nodeData = new TreeNodeData
+                {
+                    Name = node.Text,
+                    Children = GetTreeNodeData(node.Nodes).ToList()
+                };
+                nodeDataList.Add(nodeData);
+            }
+
+            return nodeDataList.ToArray();
+        }
+        //load
+        private void LoadDataToTreeView(TreeNodeData[] treeNodesData)
+        {
+            trv_Data.Nodes.Clear(); // Xoá các node cũ trước khi thêm dữ liệu mới
+
+            foreach (var nodeData in treeNodesData)
+            {
+                AddNodeToTreeView(nodeData, null);
+            }
+        }
+        private void AddNodeToTreeView(TreeNodeData nodeData, TreeNode parentNode)
+        {
+            TreeNode newNode = new TreeNode(nodeData.Name);
+
+            if (parentNode == null)
+            {
+                trv_Data.Nodes.Add(newNode);
+            }
+            else
+            {
+                parentNode.Nodes.Add(newNode);
+            }
+
+            foreach (var childNodeData in nodeData.Children)
+            {
+                AddNodeToTreeView(childNodeData, newNode);
+            }
+        }
+        
+        private TreeNodeData[] LoadTreeViewFromXml(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(TreeNodeData[]));
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    Console.Write("");
+                }
+            }
+            try
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    return (TreeNodeData[])serializer.Deserialize(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (new List<TreeNodeData>()).ToArray();
+            }
+        }
+
+        #endregion
+
+        private void SetCamGroup_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!setCamMode)
+            {
+                SaveData();
+            }
+        }
+    }
+}
+[Serializable]
+public class TreeNodeData
+{
+    public string Name { get; set; }
+    public List<TreeNodeData> Children { get; set; } = new List<TreeNodeData>();
+}
